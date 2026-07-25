51894cd94b584849ec26d41bd6ab5001a4eb9e90
manuja-xo62 <manuja.work61@gmail.com>
2026-07-26 00:24:06 +0530

    created the api route for remidate as well

--- backend_engine/app.py
+++ backend_engine/app.py
@@ -21,3 +21,32 @@ def scane_file():
         "vulnerbilities": vulnerbilities
     }
     return jsonify(payload), 200
+
+@app.route('api/remediate', methods=['POST'])
+def remediate_file():
+    data = request.get_json()
+
+    if not data or 'line' not in data or 'replacement' not in data:
+        return jsonify({"error": "Missing line or replacement data"}), 400
+    
+    line_num = int(data['line'])
+    replacement = str(data['replacement'])
+
+    scanner = DockerScanner(TARGET_FILE)
+    success = scanner.remediate(line_num, replacement)
+
+    if success:
+        updated_vulnerbilities = scanner.scan()
+        return jsonify({
+            "status": "success",
+            "remaining_issues": len(updated_vulnerbilities),
+            "vulnerbilities": updated_vulnerbilities
+        }), 200
+    
+    else:
+        return jsonify({"error": "Failed to apply fix."}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
+    
