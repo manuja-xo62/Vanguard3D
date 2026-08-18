41ad60117e8a6c57b686720d2cbfcbd0ac18b2d5
manuja-xo62 <manuja.work61@gmail.com>
2026-08-18 23:34:10 +0530

    added the backend engine to sys.path so it can be accessed without netowrk

--- Sys_new/VanguardNode_v5_Workspace/2_CLI_Action/vanguard_cli.py
+++ Sys_new/VanguardNode_v5_Workspace/2_CLI_Action/vanguard_cli.py
@@ -3,3 +3,16 @@ import sys
 import json
 import uuid
 from pathlib import Path
+
+#adding the backend to a sys.path so it doens't depend on the network
+backend_path = Path(__file__).parent.parent / "1_Backend_Engine"
+sys.path.append(str(backend_path.resolve()))
+
+try:
+    from checkov_parser import run_checkov_scan #type: ignore
+    from risk_engine import calculate_risk #type: ignore
+    from event_store import record_scan, init_db #type: ignore
+except ImportError as e:
+    print(f"FATAL: Could not load backend modules. Ensure 1_Backend_Engine exist alongside 2_CLI_Action. Error : {e}")
+    sys.exit(1)
+
